9a6ad4b2a1ad09c012d5250ac48cf2262c122e6a
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-01-09 23:23:11 +0900

    01-06. routes, controller 생성

--- practice/tdd-app/server.js
+++ practice/tdd-app/server.js
@@ -6,6 +6,8 @@ const PORT = 5000; // Express 서버를 위한 포트 설정
 
 // App
 const app = express(); // 새로운 Express 어플 생성
+const productRoutes = require('./routes');
+app.use("/api/products", productRoutes);
 app.use(express.json()); // bodyParser를 대체할 express의 미들웨어 함수
 app.get('/', (req, res) => { // '/'로 요청이 오면 Hello World를 전달
     res.send('Hello World');
@@ -13,4 +15,4 @@ app.get('/', (req, res) => { // '/'로 요청이 오면 Hello World를 전달
 
 // app.listen(PORT, HOST); // 해당 포트와 호스트에서 HTTP서버를 시작
 app.listen(PORT);
-console.log(`Running on http://${HOST}:${PORT}`);
+// console.log(`Running on http://${HOST}:${PORT}`);
